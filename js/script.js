c33466e861d921f98bddf45b67f11e7416dee1e7
Tiffany_B <97065732+tiffy3b@users.noreply.github.com>
2022-05-19 20:23:29 -0400

    added click button and repo info

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const itsMe = document.querySelector(".overview");
 const username = "tiffy3b";
 const repoList = document.querySelector(".repo-list");
+const repoInfo = document.querySelector(".repos");
+const singleRepoData = document.querySelectorAll(".repo-data");
 
 const myProfile = async function () {
     const response = await fetch(`https://api.github.com/users/${username}`);
@@ -39,4 +41,37 @@ const listClasses = function (repos) {
         listRepo.innerHTML = `<h3>${repo.name}</h3>`;
         repoList.append(listRepo);
     }
-}
+};
+repoList.addEventListener("click", function (e){
+    if (e.target.matches("h3")) {
+        const repoName = e.target.innerText;
+        singleRepoInfo(repoName);
+    }
+});
+const singleRepoInfo = async function (repoName){
+    const grabInfo = await fetch (`https://api.github.com/repos/${username}/${repoName}`);
+    const info = await grabInfo.json();
+    console.log(info);
+    const fetchLanguages = await fetch (info.languages_url);
+    const languageData = await fetchLanguages.json();
+
+    const languages = [];
+        for (const language in languageData){
+        languages.push(language);}
+
+        specificRepo(repoInfo, languages);
+};
+const specificRepo = function (repoInfo, languages){
+    singleRepoData.innerHTML = "";
+    singleRepoData.classList.remove("hide");
+    repoInfo.classList.add("hide");
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <h3>Name: ${info.name}</h3>
+        <p>Description: ${info.description}</p>
+        <p>Default Branch: ${info.default_branch}</p>
+        <p>Languages: ${languages.join(", ")}</p>
+        <a class="visit" href="${info.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>`;
+
+    singleRepoData.append(div);
+};
